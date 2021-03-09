216e5066f67c7522fa3dbf43da84fd911ad8c8af
mavel101 <marten.veldmann@hhu.de>
2021-03-09 11:40:59 +0100

    add python ismrmrd server submodule and switch to ismrm2021 branch

--- python-ismrmrd-server/docker/spiraltraj/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('spiraltraj',
-                    sources = ['gen_spiral.cpp', 'vdspiral.cpp', 'nonCartesianTraj.cpp'])
-
-setup (name = 'SpiralTraj',
-       version = '1.0',
-       description = 'This package calculate a spiral trajectory',
-       ext_modules = [module1])
--- python-ismrmrd-server/docker/spiraltraj/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import spiraltraj
-
-print('basic test: spiral out trajectory calculation with default settings:')
-res = spiraltraj.calc_traj(spiraltype=1)
-print('shape of returned list: [', len(res), ', ', len(res[0]), ']')

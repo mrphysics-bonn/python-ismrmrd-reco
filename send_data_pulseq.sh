c0608c08be1643ae491f163dc19f6f6523f4216d
Marten Veldmann <marten.veldmann@dzne.de>
2025-07-09 09:47:36 +0200

    re-add Siemens raw data conversion

--- send_data_pulseq.sh
+++ send_data_pulseq.sh
@@ -15,4 +15,12 @@ else
     OUT_FILE="$2"
 fi
 
+PARAM_PATH=${BASH_SOURCE%/*}/python-ismrmrd-server/parameter_maps
+
+if [[ $IN_FILE == *.dat ]]; then
+    rm /tmp/tmp.h5
+    siemens_to_ismrmrd -f $IN_FILE --user-map $PARAM_PATH/IsmrmrdParameterMap_Siemens_pulseq.xml --user-stylesheet $PARAM_PATH/IsmrmrdParameterMap_Siemens_pulseq.xsl -o /tmp/tmp.h5
+    IN_FILE="/tmp/tmp.h5"
+fi
+
 python python-ismrmrd-server/client.py -c bart_pulseq -o $OUT_FILE -G images $IN_FILE
